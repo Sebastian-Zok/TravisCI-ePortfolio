57b44a56465f5d34093d11f33983415836c2a178
Sebastian-Zok <sebastianpaulzok@gmail.com>
2021-12-07 08:42:52 +0100

    test

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -3,13 +3,13 @@ var assert = require('assert'),
     seriousCalculations = require('../');
 
 vows.describe('serious-calculations').addBatch({
-  'when performing serious calculations': {
-    topic: function() {
-      return seriousCalculations.performSeriousCalculations(4);
-    },
-    'result should be valid': function (topic) {
-      assert.isNumber(topic);
-      assert.equal(topic, 8);
+    'when performing serious calculations': {
+        topic: function() {
+            return seriousCalculations.performSeriousCalculations(4);
+        },
+        'result should be valid': function(topic) {
+            assert.isNumber(topic);
+            assert.equal(topic, 8);
+        }
     }
-  }
 }).export(module);
